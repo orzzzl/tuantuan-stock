e9a0eb5e671597328762ee51cb22564a9dae2ac5
Zeleng Zhuang <zelengzhuang@Zelengs-MacBook-Air.local>
2026-07-07 22:27:55 -0700

    Task 22: bundle Google font assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tuantuan_stock/app/tuantuan_stock_app.dart';
 
 void main() {
+  configureBundledFonts();
   runApp(const ProviderScope(child: TuanTuanStockApp()));
+}
+
+var _fontLicensesRegistered = false;
+
+void configureBundledFonts() {
+  GoogleFonts.config.allowRuntimeFetching = false;
+  if (_fontLicensesRegistered) return;
+  _fontLicensesRegistered = true;
+
+  LicenseRegistry.addLicense(() async* {
+    final baloo2License = await rootBundle.loadString(
+      'assets/fonts/OFL-Baloo2.txt',
+    );
+    yield LicenseEntryWithLineBreaks(<String>['Baloo 2'], baloo2License);
+
+    final zcoolLicense = await rootBundle.loadString(
+      'assets/fonts/OFL-ZCOOLKuaiLe.txt',
+    );
+    yield LicenseEntryWithLineBreaks(<String>['ZCOOL KuaiLe'], zcoolLicense);
+  });
 }
